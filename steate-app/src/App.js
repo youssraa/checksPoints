df76bfbde1536c824ca39a73f44ad6494d6c1f59
yosraT <tayachii.yosra@gmail.com>
2020-11-16 22:58:59 +0100

    state up date

--- steate-app/src/App.js
+++ steate-app/src/App.js
@@ -1,62 +1,60 @@
-import logo from './logo.svg';
+
 import './App.css';
 import React from 'react';
+import  "bootstrap/dist/css/bootstrap.min.css"
+import {Button,Card , ListGroup , ListGroupItem} from 'react-bootstrap';
+
 class App extends React.Component {
 
   constructor(props){
     super(props)
     this.state ={
       fullName :  "yosra tayachi" , 
-      bio : "blablabla" , 
-      imgSrc : "logoT.PNG" ,
+      bio : "Websites promote you 24/7: No employee will do that" , 
+      imgSrc : "logoT.PNG",
       profession : "dev web ",
-      isChecked : false ,
+      isChecked : true ,
       digit:0,
     };  
     this.handelClick =this.handelClick.bind(this);
   }
+  componentDidMount = () =>{
+    setInterval(()=>{
+      this.setState(prevState=>({digit : prevState.digit +1 ,}));
+    },1000);
+  } ;
   handelClick(){
     this.setState(state=>({
       isChecked :!state.isChecked 
     }))
   }
-  componentDidMount =()=>{
-    setInterval =(()=>{
-      this.setState(prevState =>({
-        digit: prevState.digit +1 
-      }));
-    }, 1000);
-
-     }; 
-  isDisplayed() {
-
-    var x = document.getElementById("myDIV");
-      x.style.display = "block";
-    }
-    noDisplayed() {
-
-      var x = document.getElementById("myDIV");
-        x.style.display = "none";
-      }
-
-
-  
   render(){
      return (
      <React.Fragment>
-    <button onClick={this.handelClick }> click me </button>
-    {this.state.isChecked ? this.isDisplayed() :this.noDisplayed()}
-     <div id="myDIV" >
-     <h1> {this.state.fullName}</h1>
-     <h1> {this.state.bio}</h1>
-     <h1> {this.state.imgSrc}</h1>
-     <h1> {this.state.fullName}</h1>
-     <span> counter : {this.state.digit}</span>
+       <div className="hello">
+    <Button variant="primary" onClick={this.handelClick }>{this.state.isChecked? "Hide" : "Show "} </Button>
+    {/* {this.state.isChecked ? this.isDisplayed() :this.noDisplayed()} */}
+    {this.state.isChecked && 
+    <Card style={{ width: '18rem' }}>
+  <Card.Img variant="top" src={this.state.imgSrc} />
+  <Card.Body>
+    <Card.Title>Welcome {this.state.fullName} </Card.Title>
+    <Card.Text>
+   <i>"  {this.state.bio}  " </i> 
+    </Card.Text>
+  </Card.Body>
+  <ListGroup className="list-group-flush">
+    <ListGroupItem>{this.state.profession}</ListGroupItem>
+  </ListGroup>
+  <Card.Body>
+    <Card.Link href="#">counter : {this.state.digit}</Card.Link>
+  </Card.Body>
+    </Card>
+     }
      </div>
 
      </React.Fragment>
   );
 }
-
 }
 export default App;
